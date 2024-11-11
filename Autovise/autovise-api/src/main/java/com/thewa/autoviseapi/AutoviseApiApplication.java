49a5306170ddf90745e066086f178d0226e7c5d3
Diwakar <the.walkersingh@hotmail.com>
2024-11-11 17:41:05 +0530

    Initial

---
+++ Autovise/autovise-api/src/main/java/com/thewa/autoviseapi/AutoviseApiApplication.java
@@ -0,0 +1,10 @@
+package com.thewa.autoviseapi;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoviseApiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AutoviseApiApplication.class, args);
+	}
+}
---
+++ Autovise/autovise-api/src/test/java/com/thewa/autoviseapi/AutoviseApiApplicationTests.java
@@ -0,0 +1,10 @@
+package com.thewa.autoviseapi;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoviseApiApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
